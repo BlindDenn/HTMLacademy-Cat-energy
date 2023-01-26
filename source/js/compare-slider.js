4ce87b261aaa1873bbd07cff89aa671c63357e57
BlindDenn <104034214+BlindDenn@users.noreply.github.com>
2023-01-26 15:08:25 +0000

    Deploying to gh-pages from @ BlindDenn/HTMLacademy-Cat-energy@cd186e2c82cce163a9cd39dac70827b1daf4a11b 🚀

--- source/js/compare-slider.js
+++
@@ -1,6 +0,0 @@
-let sliderKnob = document.querySelector('.compare-slider__control-knob-group');
-let sliderGroup = document.querySelector('.compare-slider');
-
-sliderKnob.oninput = function () {
-  sliderGroup.style.setProperty('--knob-position', sliderKnob.value + `%`);
-}
